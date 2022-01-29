59018eda115505a2052cea0fd84f91857d577af3
Maxime Legare <mleg1234@protonmail.com>
2022-01-29 10:38:05 -0500

    can add songs to db

--- pages/add-songs.jsx
+++ pages/add-songs.jsx
@@ -1,44 +1,30 @@
 import React from "react";
-import { useRef } from "react";
-import axios from "axios";
 export default function AddMusic() {
-
-  const inputRef = useRef(null)
-
   const handleChangeFiles = async (e) => {
-    const formData = new FormData()
+    const formData = new FormData();
 
-
-    for (const file of inputRef.current.files){
-      formData.append("files", file) 
+    for (const file of e.target.files) {
+      formData.append("files", file);
     }
 
-    for (const [key, value] of formData){
-      console.log(`key ${key}`)
-      console.log(`value ${value}`)
-    }
+    // Post to backend with formData containing the files
+    fetch("http://localhost:3000/api/add-songs", {
+      method:"POST",
+      body:formData
 
-     axios.post("http://localhost:3000/api/add-songs", formData)
+    }).catch((err) => {
+      console.log(err)
+    })
+
   };
-  
-
 
   return (
     <div>
       <div>
-        <input type="file" multiple ref={inputRef}
-        onChange={handleChangeFiles} 
-        />
-        <button >upload</button>
+        <input type="file" multiple onChange={handleChangeFiles} />
+        <button>upload</button>
       </div>
       <div className="fileList">
-        {/* {
-          files?.map((file) => {
-            <div>
-              {file?.name}
-            </div>
-          })
-        } */}
       </div>
     </div>
   );
--- pages/api/add-songs.js
+++ pages/api/add-songs.js
@@ -1,25 +1,39 @@
-import mm from "music-metadata";
+import mm from "music-metadata/";
 import multer from "multer";
+
+import { sql_query } from "../../lib/db";
 
 export const config = {
   api: {
     bodyParser: false,
   },
 };
-
+// Library that receive files
 const upload = multer();
 
 export default function handler(req, res) {
   upload.any()(req, {}, (err) => {
     const files = req.files;
-    if(err){
-      throw err
+    if (err) {
+      throw err;
     }
-    files.forEach(async (file) => {
-      const metadata = await mm.parseBuffer(file.buffer, file.mimetype)
 
-    })
-    
+    // Loop through files
+    files.forEach(async (file) => {
+      // Get the metadata of the file with it's buffer
+      const {
+        common: { artist, title, album },
+      } = await mm.parseBuffer(file.buffer, file.mimetype);
+
+      try {
+        await sql_query(
+          `INSERT INTO songs (title, artist, album)
+         VALUES ('${title}', '${artist}', '${album}')`
+        );
+      } catch (e) {
+        console.log(e)
+      }
+    });
   });
   res.status(200).json({ message: "done" });
 }
