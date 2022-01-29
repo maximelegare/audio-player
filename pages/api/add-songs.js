4bf37559cece171e5f05db8cf3167db3000dba3d
Maxime Legare <mleg1234@protonmail.com>
2022-01-28 21:35:19 -0500

    can acces metadata in backend

--- pages/api/add-songs.js
+++ pages/api/add-songs.js
@@ -1,20 +1,25 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import mm from "music-metadata";
-// import { makeTokenizer } from "@tokenizer/http";
 import mm from "music-metadata";
 import multer from "multer";
-
 
 export const config = {
   api: {
     bodyParser: false,
   },
-}
+};
 
-const upload = multer()
+const upload = multer();
 
-export default function handler (req, res) {
-  upload.any()(req, {}, err =>{
-    console.log(req.files)
-  })
+export default function handler(req, res) {
+  upload.any()(req, {}, (err) => {
+    const files = req.files;
+    if(err){
+      throw err
+    }
+    files.forEach(async (file) => {
+      const metadata = await mm.parseBuffer(file.buffer, file.mimetype)
+
+    })
+    
+  });
+  res.status(200).json({ message: "done" });
 }
