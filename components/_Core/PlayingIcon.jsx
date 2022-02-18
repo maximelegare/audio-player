97d3db2ca2d218540bdc08a2bf1af8e77d8a98aa
Maxime Legare <mleg1234@protonmail.com>
2022-02-18 16:02:23 -0500

    add pause to animation when music is not playing

--- components/_Core/PlayingIcon.jsx
+++ components/_Core/PlayingIcon.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 import styles from "../../styles/_Core/PlayingIcon.module.scss";
 
+import { isPlayingState } from "../../atoms/audioAtom";
+import { useRecoilValue } from "recoil";
 const PlayingIcon = () => {
 
+  const isPlaying = useRecoilValue(isPlayingState)  
 
   return (
     <div className={styles.container}>
-      <span className={styles.col}></span>
-      <span className={styles.col}></span>
-      <span className={styles.col}></span>
+      <span className={`${styles.col} ${!isPlaying && styles.paused}`}></span>
+      <span className={`${styles.col} ${!isPlaying && styles.paused}`}></span>
+      <span className={`${styles.col} ${!isPlaying && styles.paused}`}></span>
     </div>
   );
 };
