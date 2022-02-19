f173bc63c9056fd1563c1305f25f391094ba287e
Maxime Legare <mleg1234@protonmail.com>
2022-02-19 10:16:25 -0500

    create custom hook with audio logic

--- components/AudioPlayer/AudioPlayer.jsx
+++ components/AudioPlayer/AudioPlayer.jsx
@@ -7,77 +7,20 @@ import InfosAlbumPlaying from "./InfosAlbumPlaying";
 import AudioControlesCenter from "./AudioControlesCenter";
 
 import VolumeControles from "./VolumeControles";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { isPlayingState } from "../../atoms/audioAtom";
 
-import { currentSongState, currentPlaylistState } from "../../atoms/audioAtom";
+import { useAudioPlayer } from "../../hooks/AudioHooks";
 
 const AudioPlayer = ({ fileUrl, title, artist, album, imgUrl, duration }) => {
-  // Global state
-  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
-  const [currentSong, setCurrentSong] = useRecoilState(currentSongState);
-  const currentPlaylist = useRecoilValue(currentPlaylistState);
-
-  // Progress input vales
-  const [progressInput, setRangeInput] = useState({
-    values: [0],
-    min: 0,
-    step: 1,
-  });
-
-  const [max, setMax] = useState(100); // maximum time of the range input (duration of the song)
-
-  // references
-  const animationRef = useRef(null); // Animation of the Range input
-
-  const audioPlayer = useRef(null); // AudioPlayer which is in child component
-
-  // set the max duration when metadata are loaded
-  useEffect(() => {
-    const seconds = Math.floor(duration);
-    setMax(seconds);
-  }, [audioPlayer.current?.loadedmetadata, duration]);
-
-  // animation that updates the range while it's playing
-  const whilePlaying = () => {
-    setRangeInput({
-      ...progressInput,
-      values: [audioPlayer.current.currentTime],
-    });
-
-    // call itself to always do the animation
-    animationRef.current = requestAnimationFrame(whilePlaying);
-  };
-
-  // Set play/pause based on isPlaying value & when file changes
-  useEffect(() => {
-    if (isPlaying) {
-      audioPlayer.current.play();
-      animationRef.current = requestAnimationFrame(whilePlaying); // Start range input animation
-    } else {
-      audioPlayer.current.pause();
-      cancelAnimationFrame(animationRef.current); // Stop range input animation
-    }
-  }, [isPlaying, fileUrl]);
-
-  useEffect(() => {
- 
-    if (Math.round(audioPlayer.current.currentTime) === duration) {
-      setCurrentSong({
-        ...currentPlaylist[currentSong.songIdx + 1],
-        songIdx: currentSong.songIdx + 1,
-      });
-    }
-  }, [audioPlayer.current?.currentTime, duration]);
+  const {
+    isPlaying,
+    setIsPlaying,
+    progressInput,
+    max,
+    audioPlayer,
+    changeRange
+  } = useAudioPlayer(fileUrl, duration);
 
 
-
-
-  // when a user drag the knob, it updates the progress-bar
-  const changeRange = (values) => {
-    setRangeInput({ ...progressInput, values: [values] });
-    audioPlayer.current.currentTime = values;
-  };
 
   return (
     <div className={styles.audioPlayer}>
---
+++ hooks/AudioHooks.js
@@ -0,0 +1,101 @@
+import { useState, useRef, useEffect } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { currentPlaylistState, currentSongState, isPlayingState } from "../atoms/audioAtom";
+
+
+const useAudioPlayer = (fileUrl, duration) => {
+  // Global state
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  const [currentSong, setCurrentSong] = useRecoilState(currentSongState);
+  const currentPlaylist = useRecoilValue(currentPlaylistState);
+
+  // Progress input vales
+  const [progressInput, setRangeInput] = useState({
+    values: [0],
+    min: 0,
+    step: 1,
+  });
+
+  const [max, setMax] = useState(100); // maximum time of the range input (duration of the song)
+
+
+  // Dom references
+  const animationRef = useRef(null); // Animation of the Range input
+
+  const audioPlayer = useRef(null); // AudioPlayer which is in child component
+
+
+
+
+  // set the max duration when metadata are loaded
+  useEffect(() => {
+    const seconds = Math.floor(duration);
+    setMax(seconds);
+  }, [audioPlayer.current?.loadedmetadata, duration]);
+
+
+
+
+  // animation that updates the range while it's playing
+  const whilePlaying = () => {
+    setRangeInput({
+      ...progressInput,
+      values: [audioPlayer.current.currentTime],
+    });
+
+    // call itself to always do the animation
+    animationRef.current = requestAnimationFrame(whilePlaying);
+  };
+
+
+
+
+  // Set play/pause based on isPlaying value & when file changes
+  useEffect(() => {
+    if (isPlaying) {
+      audioPlayer.current.play();
+      animationRef.current = requestAnimationFrame(whilePlaying); // Start range input animation
+    } else {
+      audioPlayer.current.pause();
+      cancelAnimationFrame(animationRef.current); // Stop range input animation
+    }
+  }, [isPlaying, fileUrl]);
+
+
+
+
+   // Sets the next song when the previous song finished
+   useEffect(() => {
+    if (Math.round(audioPlayer.current.currentTime) === duration) {
+      setCurrentSong({
+        ...currentPlaylist[currentSong.songIdx + 1],
+        songIdx: currentSong.songIdx + 1,
+      });
+    }
+  }, [audioPlayer.current?.currentTime, duration]);
+
+
+
+
+   // when a user drag the knob, it updates the progress-bar
+   const changeRange = (values) => {
+    setRangeInput({ ...progressInput, values: [values] });
+    audioPlayer.current.currentTime = values;
+  };
+
+
+  return {
+    isPlaying,
+    setIsPlaying,
+    currentSong,
+    setCurrentSong,
+    progressInput,
+    max,
+    currentPlaylist,
+    audioPlayer,
+    animationRef,
+    changeRange
+  };
+};
+
+export { useAudioPlayer };
