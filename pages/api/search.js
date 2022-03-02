b6dc3524487bac2004e443104970e8551f99b634
Maxime Legare <mleg1234@protonmail.com>
2022-03-02 15:45:02 -0500

    cleaning of package.json

--- pages/api/search.js
+++ pages/api/search.js
@@ -6,10 +6,10 @@ const handler = async (req, res) => {
   try {
     const response = await db.query(
       `
-  SELECT s.title, s.title_route as song_route, s.liked,  s.album, s.track_no, s.streaming_url, a.picture_url, a.artist, s.duration, a.title_route as album_route, a.artist_route 
-  FROM songs s
-  JOIN albums a
-  ON s.album = a.title   WHERE s.title LIKE '%${req.query.q}%' LIMIT 5
+    SELECT s.title, s.title_route as song_route, s.liked,  s.album, s.track_no, s.streaming_url, a.picture_url, a.artist, s.duration, a.title_route as album_route, a.artist_route 
+    FROM songs s
+    JOIN albums a
+    ON s.album = a.title   WHERE s.title LIKE '%${req.query.q}%' LIMIT 5
     `
     );
 
