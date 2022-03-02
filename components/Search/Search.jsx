ec05a15f8c472d033c7646af4105528dbd62d5e9
Maxime Legare <mleg1234@protonmail.com>
2022-03-02 15:37:30 -0500

    separate search input from background => can click on background to close the search

--- components/Search/Search.jsx
+++ components/Search/Search.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import styles from "../../styles/Search/Search.module.scss";
 import CustomInput from "../_Core/CustomInput/CustomInput";
 import PageLayout from "../Layout/PageLayout";
-
-
+import { searchSectionVisibilityState } from "../../atoms/visibilityAtom";
+import { useSetRecoilState } from "recoil";
 
 const Search = () => {
   const [suggestions, setSuggestions] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  const setSearchVisibility = useSetRecoilState(searchSectionVisibilityState);
 
   const handleKeyUp = async (e) => {
     const { value } = e.target;
@@ -26,8 +27,8 @@ const Search = () => {
   };
   return (
     <div className={styles.fixed}>
-      <PageLayout>
-        <div style={{ marginTop: "20px" }}>
+      <div className={styles.input} style={{marginTop:"20px"}}>
+        <PageLayout>
           <form autoComplete="false" onSubmit={(e) => e.preventDefault()}>
             <CustomInput
               placeHolder="Search Anything"
@@ -37,8 +38,12 @@ const Search = () => {
               list={suggestions}
             />
           </form>
-        </div>
-      </PageLayout>
+        </PageLayout>
+      </div>
+      <div
+        className={styles.background}
+        onClick={() => setSearchVisibility(false)}
+      ></div>
     </div>
   );
 };
