befebd8eae8444b385596863649207e78f8e65ce
Maxime Legare <mleg1234@protonmail.com>
2022-03-02 21:50:31 -0500

    remove section when there is no data found in it (inside CustomInputDropdownItems)

--- components/_Core/CustomInput/CustomInputDropdownItems.jsx
+++ components/_Core/CustomInput/CustomInputDropdownItems.jsx
@@ -1,34 +1,40 @@
 import React from "react";
 import RowListElement from "../../_Partials/List/RowList/RowListElement";
 
-import styles from "../../../styles/_Core/CustomInput.module.scss"
+import styles from "../../../styles/_Core/CustomInput.module.scss";
 
 const CustomInputDropdownListTitle = ({ title, data }) => {
   return (
     <>
-      {title && (
-        <div>
-          <h3>{title}</h3>
-          <hr />
-        </div>
+      {
+      // If there is Data, render it
+      data.length !== 0 && (
+        <>
+          {title && (
+            <div>
+              <h3>{title}</h3>
+              <hr />
+            </div>
+          )}
+          <div className={styles.customInputDropdownSections}>
+            {data.map((song) => (
+              <RowListElement
+                key={song.title}
+                song={song}
+                options={{
+                  imgWidth: 35,
+                  imgHeight: 35,
+                  noHighlightWhenClicked: true,
+                  noOptionsIcon: true,
+                  condensed: true,
+                  noAlbum: true,
+                  noDuration: true,
+                }}
+              />
+            ))}
+          </div>
+        </>
       )}
-      <div className={styles.customInputDropdownSections}>
-        {data.length !== 0 ? data.map((song) => (
-          <RowListElement
-            key={song.title}
-            song={song}
-            options={{
-              imgWidth: 35,
-              imgHeight: 35,
-              noHighlightWhenClicked: true,
-              noOptionsIcon: true,
-              condensed: true,
-              noAlbum: true,
-              noDuration: true,
-            }}
-          />
-        )) : <div>No {title} Found</div>}
-      </div>
     </>
   );
 };
