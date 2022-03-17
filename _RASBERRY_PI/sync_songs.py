a258e58283c4c221d06cc2a4227dadeb8eb14433
Maxime Legare <mleg1234@protonmail.com>
2022-03-17 13:59:36 -0400

    work on sync_songs.py (add mysqk connector)

--- _RASBERRY_PI/sync_songs.py
+++ _RASBERRY_PI/sync_songs.py
@@ -1,28 +1,47 @@
-import os;
+import os
 from tinytag import TinyTag
+import mysql.connector
+
 
 directory = "C:/Users/maxle/Desktop/audioPlayer/audio-player/_musicTest"
-    
-for file in os.listdir(directory):
-     filename = os.fsdecode(file)
-     filepath = f"{directory}/{filename}"
-     tag = TinyTag.get(filepath)  
-     
-     route = f"/{tag.artist}/{tag.album}/{tag.title}"
-     
-     song = {
-         "title":tag.title,
-         "album":tag.album,
-         "RP_streaming_path":f"/{file}",
-         "title_route":route,
-         "track_no":tag.track,
-         "album_track_no":tag.track_total,
-         "duration":tag.duration,
-         "liked":0,
-     }
-     print(song)
-     
-              
 
-     
-     
+config = {"user": 'xxxxxxx',
+          "password": 'xxxxxxxxx',
+          "host": 'xxxxxxx',
+          "database": 'xxxxxxxx'}
+
+cnx = mysql.connector.connect(config)
+
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    filepath = f"{directory}/{filename}"
+    tag = TinyTag.get(filepath, image=True)
+    
+
+    # Route for the app
+    songRoute = f"/{tag.artist}/{tag.album}/{tag.title}"
+
+    # Song name for the file &
+    songName = f"{tag.artist} - {tag.album} - {tag.title}"
+    albumName = f"{tag.artist} - {tag.album}"
+
+    # Checks if the album exists in Album table 
+    try:
+        cursor = cnx.cursor()
+        cursor.execute(f"""EXISTS(SELECT 1 FROM albums WHERE title = '{tag.album}'""")
+    finally:
+        cnx.close()    
+
+
+    song = {
+        "title": tag.title,
+        "album": tag.album,
+        "RP_streaming_path": f"/{file}",
+        "title_route": songRoute,
+        "track_no": tag.track,
+        "album_track_no": tag.track_total,
+        "duration": tag.duration,
+        "liked": 0,
+    }
+    
