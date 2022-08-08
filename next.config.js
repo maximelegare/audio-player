e71d700085b32fda2afef4f450898021bccd139d
Maxime Legare <mleg1234@protonmail.com>
2022-08-07 22:36:25 -0400

    add vercel.json for handling cors

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ module.exports = withPWA({
     skipWaiting:true,
     disable:process.env.NODE_ENV === 'development'
   },
+
   images:{
     domains:["res.cloudinary.com", "firebasestorage.googleapis.com", "i.scdn.co"]
   },
