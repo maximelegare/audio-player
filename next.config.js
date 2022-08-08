64ac6fce86d9909a384ec5bc9ebae6032395cb8a
Maxime Legare <mleg1234@protonmail.com>
2022-08-07 22:44:59 -0400

    handle cors w/ next.config

--- next.config.js
+++ next.config.js
@@ -9,7 +9,6 @@ module.exports = withPWA({
     skipWaiting:true,
     disable:process.env.NODE_ENV === 'development'
   },
-
   images:{
     domains:["res.cloudinary.com", "firebasestorage.googleapis.com", "i.scdn.co"]
   },
@@ -33,6 +32,20 @@ module.exports = withPWA({
         secure: true
       }
     }
+    
   },
-  
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 });
